d4d8eea84048f5747b492c9563a8753952ed5dde
nurulhjsa <nurulhasanahbule.nh@gmail.com>
2018-01-05 10:22:45 +0700

    Method Panggil

---
+++ Method1.java
@@ -0,0 +1,12 @@
+public class Method1
+{
+    public static void sayHai(String Nama, Integer Umur)
+    {
+        System.out.println("Halo nama Saya " +Nama+ ", Umur Saya " +Umur);
+    }
+
+    public static void main(String[] args)
+    {
+        sayHai("Nurul Hjsa",22);
+    }
+}
