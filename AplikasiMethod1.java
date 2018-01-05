82f6e218e3854e3410d1287b485188078399964c
nurulhjsa <nurulhasanahbule.nh@gmail.com>
2018-01-05 10:34:49 +0700

    Aplikasi Method

---
+++ AplikasiMethod1.java
@@ -0,0 +1,6 @@
+public class AplikasiMethod1
+{
+    public static void main(String[] args){
+        AplikasiMethod2.Perkenalan("Nurul Hjsa", 22);
+    }
+}
---
+++ AplikasiMethod2.java
@@ -0,0 +1,6 @@
+public class AplikasiMethod2
+{
+    public static void Perkenalan(String nama, Integer umur){
+        System.out.println("Nama Saya "+nama+ ", Umur Saya"+umur);
+    }
+}
